b03d70953d4c2a6b77cbbd48127a59f2d4210752
PhuzionMeep <tug03794@temple.edu>
2019-04-14 00:49:54 -0400

    add sign up page

---
+++ js/login.js
@@ -0,0 +1,20 @@
+$( document ).ready(function() {
+  /*   $.ajax({
+        url: 'http://api.reimaginebanking.com/customers?key=14e76db44c0d76ef32abacebb8f5c903',
+        success: function(results){
+            alert("work");
+            console.log(results);
+        }
+    }); */
+    // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCweTYap4IJhr5BZE70I5vKp8g10Yj4QXk",
+    authDomain: "bitcamp-tudev.firebaseapp.com",
+    databaseURL: "https://bitcamp-tudev.firebaseio.com",
+    projectId: "bitcamp-tudev",
+    storageBucket: "bitcamp-tudev.appspot.com",
+    messagingSenderId: "548659393485"
+  };
+  firebase.initializeApp(config);
+
+});
--- login.js
+++
@@ -1,9 +0,0 @@
-$( document ).ready(function() {
-    $.ajax({
-        url: 'http://api.reimaginebanking.com/customers?key=14e76db44c0d76ef32abacebb8f5c903',
-        success: function(results){
-            alert("work");
-            console.log(results);
-        }
-    });
-});
